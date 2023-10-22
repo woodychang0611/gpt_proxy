165b5bf97e773b62bcdf6a4f27ba3e105c905246
張天慈 <woodychang0611@gmail.com>
2023-10-22 20:54:36 +0800

    update docker_push

--- docker_push.ps1
+++ docker_push.ps1
@@ -1,13 +1,11 @@
 
 $commit_id = git rev-parse HEAD
-$commit_id 
-
+Write-Host "Commit Id: $commit_id"
 $json_location = Join-Path $PSScriptRoot "commit_id.json"
-$content = "{""commit_id"":""$commit_id""}" 
-$json_location 
-$content
+$content = "{""commit_id"":""$commit_id""}"
+Write-Host "Save to $json_location "
 Set-Content -Path $json_location  -Value $content -Encoding Ascii #Use Ascii to save without BOM
-return
+
 docker build -t gpt_proxy .
 docker tag gpt_proxy:latest woodychang0611/gpt_proxy:latest
 docker push woodychang0611/gpt_proxy:latest
