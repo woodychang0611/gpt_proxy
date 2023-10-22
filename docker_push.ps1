84f304622a359aa7a17941f4c7212b5fe41f5c8e
張天慈 <woodychang0611@gmail.com>
2023-10-22 20:58:07 +0800

    update

--- docker_push.ps1
+++ docker_push.ps1
@@ -6,6 +6,8 @@ $content = "{""commit_id"":""$commit_id""}"
 Write-Host "Save to $json_location "
 Set-Content -Path $json_location  -Value $content -Encoding Ascii #Use Ascii to save without BOM
 
+#Build docker image
 docker build -t gpt_proxy .
 docker tag gpt_proxy:latest woodychang0611/gpt_proxy:latest
+#Push to docker Hub
 docker push woodychang0611/gpt_proxy:latest
