ac0e503e36030db1ab5a8ec78d7b4e554e6c832c
張天慈 <woodychang0611@gmail.com>
2024-01-14 22:24:58 +0800

    update docker build with no cache

--- docker_push.ps1
+++ docker_push.ps1
@@ -8,7 +8,7 @@ Write-Host "Save to $json_location "
 Set-Content -Path $json_location  -Value $content -Encoding Ascii #Use Ascii to save without BOM
 
 #Build docker image
-docker build -t gpt_proxy .
+docker build --no-cache -t gpt_proxy .
 docker tag gpt_proxy:latest woodychang0611/gpt_proxy:latest
 #Push to docker Hub
 docker push woodychang0611/gpt_proxy:latest
