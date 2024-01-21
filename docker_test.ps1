68bdb1a51733dad602b13887b534ff2707ac7d33
張天慈 <woodychang0611@gmail.com>
2024-01-21 22:14:45 +0800

    update docker

--- docker_test.ps1
+++ docker_test.ps1
@@ -1,6 +1,5 @@
-#clean up build
-Remove-Item $PSScriptRoot/build -Recurse -Force
+
 docker build -t gpt_proxy .
 docker rm my_gpt_proxy -f
-docker run --name my_gpt_proxy -p 80:80 gpt_proxy
+docker run --rm --name my_gpt_proxy -p 80:80 gpt_proxy
 
